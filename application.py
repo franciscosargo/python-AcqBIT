a8490d41be1681c678d10250d619025ecf89d237
franciscosargo <fmsargo@gmail.com>
2019-01-13 17:16:32 +0000

    Add I/O initialization. Add function to open h5file correctly

--- application.py
+++ application.py
@@ -50,6 +50,64 @@ if 0:
      import re
      import subprocess
 
+def open_h5file(macAddress, nSamples, path_to_save):
+    """ 
+    Utility function to open and setup a maximum 2 hours duration h5 file for the acquisition
+    """
+
+    # Open file
+    filename = time.strftime("test_%Y-%m-%d_%H-%M-%S", time.gmtime()) + '.h5'
+    file_path = os.path.join(path_to_save, filename)
+
+    # Open file
+    f = h5.File(file_path, 'w')
+
+    # Create the datasets according to OpenSignals standard
+    root_group_name = macAddress + '/'
+    r_grp = f.create_group(root_group_name)
+
+    # Digital
+    r_grp.create_dataset('digital/digital_1', dtype='uint16', shape=(nSamples, 1), maxshape=(2*60*60*nSamples, 1), chunks=(1024, 1))
+    r_grp.create_dataset('digital/digital_2', dtype='uint16', shape=(nSamples, 1), maxshape=(2*60*60*nSamples, 1), chunks=(1024, 1))
+    r_grp.create_dataset('digital/digital_3', dtype='uint16', shape=(nSamples, 1), maxshape=(2*60*60*nSamples, 1), chunks=(1024, 1))
+    r_grp.create_dataset('digital/digital_4', dtype='uint16', shape=(nSamples, 1), maxshape=(2*60*60*nSamples, 1), chunks=(1024, 1))
+
+    # Analog
+    r_grp.create_dataset('raw/channel_1', dtype='uint16', shape=(nSamples, 1), maxshape=(2*60*60*nSamples, 1), chunks=(1024, 1))
+    r_grp.create_dataset('raw/channel_2', dtype='uint16', shape=(nSamples, 1), maxshape=(2*60*60*nSamples, 1), chunks=(1024, 1))
+    r_grp.create_dataset('raw/channel_3', dtype='uint16', shape=(nSamples, 1), maxshape=(2*60*60*nSamples, 1), chunks=(1024, 1))
+    r_grp.create_dataset('raw/channel_4', dtype='uint16', shape=(nSamples, 1), maxshape=(2*60*60*nSamples, 1), chunks=(1024, 1))
+    r_grp.create_dataset('raw/channel_5', dtype='uint16', shape=(nSamples, 1),maxshape=(2*60*60*nSamples, 1), chunks=(1024, 1))
+    r_grp.create_dataset('raw/channel_6', dtype='uint16', shape=(nSamples, 1), maxshape=(2*60*60*nSamples, 1), chunks=(1024, 1))
+    r_grp.create_dataset('raw/nSeq', dtype='uint16', shape=(nSamples, 1),  maxshape=(2*60*60*nSamples, 1), chunks=(1024, 1))
+
+    # Events
+    #r_grp.create_dataset('events/digital', dtype='uint32', shape= maxshape=(2*60*60*nSamples, 4), chunks=(512, 4))
+    #r_grp.create_dataset('raw/sync', dtype='uint32', maxshape=(2*60*60*nSamples, 4), chunks=(512, 4))
+
+    return f
+
+def write_h5file(macAddress, file_object, signal):
+    """ 
+    Utility function to open and write to a previously opened h5 file for the acquisition
+    """
+
+    # Create the datasets according to OpenSignals standard
+    root_group_name = macAddress + '/'
+    r_grp = file_object[root_group_name]
+
+    # Extract channels information
+    digital_1 = signal[:, 0]
+    digital_2 = signal[:, 1]
+    digital_3 = signal[:, 2]
+    digital_4 = signal[:, 4]
+
+
+    # 
+
+    # 
+
+
 
 def _process(macAddress, setup):
     """
@@ -87,13 +145,9 @@ def _process(macAddress, setup):
         device.start(samplingRate, acqChannels)
         device.socket.settimeout(5)
 
-        # Open file for I/O
-        filename = time.strftime("%a, %d %b %Y %H %M %S +0000", time.gmtime()) + '.h5'
-
         # Acquisition Loop
-        with h5.File(os.path.join(path_to_save, filename)) as f:
-            dataset = f.create_dataset('ds', shape=(nSamples, nChannels + 1),
-                                        maxshape=(2*60*60*nSamples, nChannels + 1))  # create dataset with maximum size of 2 hours
+        with open_h5file(macAddress, nSamples, path_to_save) as f:
+             # create dataset with maximum size of 2 hours
 
             for i in xrange(0, 2*60*60):
                 try:
