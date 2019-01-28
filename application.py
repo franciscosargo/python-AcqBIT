f448fcce50dccb94bfaaad531dafeb8a1d19bf8c
franciscosargo <fmsargo@gmail.com>
2019-01-28 23:36:52 +0000

    Add bitalino_process module, to diminish verbose

--- application.py
+++ application.py
@@ -25,147 +25,17 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 # Native
-import datetime
 import os
 import multiprocessing as mp
 import json
 
 # Third Party
-import numpy as np
-import bitalino as bt
 import pystray
 from pystray import MenuItem as item
-
-# Local
-import int_out as io
 from PIL import Image
 
-
-def __find_bitalino(macAddress, deviceName, general_event, specific_event):
-        """
-		Loop to find and connect to the bitalino device by macAddress.
-	    """
-        
-        ## Connection Loop
-        while True:
-                    
-            try:
-                ## Check for event interruption
-                if (specific_event.is_set() or general_event.is_set()):
-                    raise ValueError('Closing the acquisition.')
-
-                device = bt.BITalino(macAddress, timeout=100)  # connect to BITalino
-                print 'Running! -- NAME: {} -- ADDR: {}'.format(deviceName, macAddress)
-
-                break
-
-            except ValueError as e:
-                print '{} -- NAME: {} -- ADDR: {}'.format(e, deviceName, macAddress)
-                return None
-            
-            except Exception as e:
-                print '{} -- NAME: {} -- ADDR: {}'.format(e, deviceName, macAddress)
-                pass
-
-        return device
-
-
-def __read_bitalino(device, path_to_save, macAddress, deviceName, setup,
-                    acqChannels, acqLabels, digitalOutput, 
-                    nSamples, master_flag,
-                    sync_datetime, i_datetime, 
-                    specific_event, general_event):
-        """
-		Loop to continuously read the channels from a connected bitalino device according to input configuration.
-	    """
-
-        # Acquisition Loop
-        with io.open_h5file(path_to_save,  macAddress, acqChannels, acqLabels, nSamples) as f:
-
-            # Acquisition iteration
-            for i in xrange(0, 2*60*60):
-
-                try:
-                    dataAcquired = device.read(nSamples) 
-                    io.write_h5file(f, macAddress, dataAcquired, acqChannels, nSamples)
-
-                    # Check for synchronization
-                    datetime_now = datetime.datetime.now()
-
-                    ## Check for event interuption
-                    if (specific_event.is_set() or general_event.is_set()):
-                        raise ValueError('Closing the acquisition.')
-                    
-                    if  datetime_now - i_datetime >= sync_datetime and setup['master']:
-                        
-                        # Change digital output in master device
-                        digitalArray = [int(not bool(dg_val))
-                                        for dg_val in digitalOutput]
-                        device.trigger(digitalArray=digitalArray)
-                        digitalOutput = digitalArray
-
-                        ## Save sync_time on hdf file (TO DO: should it be time_now?)
-                        io.write_sync_datetime(f, datetime_now)
-
-                except Exception as e:
-                    io.create_opensignals_mdata(f, setup, i_datetime, i)
-                    print '{} -- NAME: {} -- ADDR: {}'.format(e, deviceName, macAddress)
-                    break
-
-
-def _process(path_to_save, macAddress, setup, general_event, specific_event):
-    """
-		Main logic for the acquisition loop.
-	"""
-
-    # Set metadata for the acquisition
-    batteryThreshold = setup['batteryThreshold']
-    acqChannels = setup['acqChannels']
-    acqLabels = setup['acqLabels']
-    samplingRate = setup['samplingRate']
-    nSamples = setup['nSamples']
-    digitalOutput = setup['digitalOutput']
-    syncInterval = setup['syncInterval']  # minutes
-    deviceName = setup['deviceName']
-    resolution = setup['resolution']
-    master_flag = setup['master']
-
-    sync_datetime = datetime.timedelta(minutes=syncInterval)
-    nChannels = len(digitalOutput) + len(acqChannels)
-
-    ## Add folder for macAddress
-    path_to_save = os.path.join(path_to_save, deviceName)
-    if not os.path.exists(path_to_save):
-        os.makedirs(path_to_save)
-
-    while True:
-
-        device = __find_bitalino(macAddress, deviceName, general_event, specific_event)
-
-        ## Check for event interruption
-        if (specific_event.is_set() or general_event.is_set()):
-            break   
-                
-        # Start Acquisition
-        device.start(samplingRate, acqChannels)
-        device.socket.settimeout(5)
-
-        # Get initial time of acquisition
-        i_datetime_acq = datetime.datetime.now()
-        i_datetime = i_datetime_acq
-
-        # Read from device
-        __read_bitalino(device, path_to_save, macAddress, deviceName,
-                        setup, acqChannels, acqLabels, digitalOutput, 
-                        nSamples, master_flag,
-                        sync_datetime, i_datetime, 
-                        specific_event, general_event)
-
-        device.close()  ## close device
-
-        ## Check for event interuption
-        if (specific_event.is_set() or general_event.is_set()):
-            break  
+# local
+from bitalino_process import _process
             
 
 # Icon methods
