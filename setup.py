b6afbaa1ee4fefb1b612a69d677746e065b6db0e
franciscosargo <fmsargo@gmail.com>
2019-01-30 16:38:12 +0000

    Update exemplary configuration file

--- setup.py
+++ setup.py
@@ -3,23 +3,18 @@ import os
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["os", "bitalino", "multiprocessing",
-                                 "json", "numpy", "pystray", "datetime", 
-                                 "PIL", "serial", "UserList", "UserString"],
+build_exe_options = {"includes": ['numpy.core._methods', 'numpy.lib.format', 'UserList', 'UserString'],
+                     "packages":['pystray', 'PIL'],
                      "include_files": ['BITALINO-logo.png', 'config.json'],
                      "include_msvcr": True}
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
 base = None
-#if sys.platform == "win32":
-    #base = "Win32GUI"
+
 
 setup(name = "acqBIT",
       version = "0.1",
       description = "Application for robust pervasive bio-signals acquisition with BITalino devices",
-      options = {"build_exe_options": build_exe_options},
-      executables = [Executable("application.py",
-                                shortcutName="acqBIT",
-                                shortcutDir="StartupFolder",
-                                base=base)])
+      options = {"build_exe": build_exe_options},
+      executables = [Executable("application.py")])
